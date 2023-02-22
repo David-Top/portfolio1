42094225183bb820a9c449edb4d2b2d52491bf46
David-Top <david.topanto@students.unnes.ac.id>
2023-02-22 21:33:29 +0700

    create Plan component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white'>
             <div className='md:left-[10%] max-w-[1100px] m-auto absolute p-4'>
                 <p>All Inclusive</p>
-                <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl'>Private Beaches and Gateway</h1>
+                <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl'>Wonderful Indonesia</h1>
                 <p className='max-w-[660px] drop-shadow-2xl py-2 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non harum est, eos aspernatur officia expedita. Pariatur, magni atque aliquam corporis quibusdam labore laboriosam fugiat, expedita eius recusandae dolorem facilis asperiores!</p>
                 <button className='bg-white text-black font-bold'>Reserve Now</button>
             </div>
---
+++ src/components/Plan.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Plan = () => {
+  return (
+    <div className='max-w-[1440px] m-auto py-16 px-4 grid lg:grid-cols-2 gap-4'>
+        {/* left side */}
+        <div className='grid grid-cols-2 grid-rows-6 h-[80vh]'>
+            <img className='object-cover w-full h-full p-2 row-span-3' src="https://images.pexels.com/photos/1643130/pexels-photo-1643130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+            <img className='object-cover w-full h-full p-2 row-span-2' src="https://images.pexels.com/photos/2499777/pexels-photo-2499777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+            <img className='object-cover w-full h-full p-2 row-span-2' src="https://images.pexels.com/photos/1555581/pexels-photo-1555581.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+            <img className='object-cover w-full h-full p-2 row-span-3' src="https://images.pexels.com/photos/837745/pexels-photo-837745.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+            <img className='object-cover w-full h-full p-2 row-span-2' src="https://images.pexels.com/photos/161293/prambanan-temple-java-hinduism-161293.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+        </div>
+
+        {/* right side */}
+        <div>
+            <h3>Plan Your Trip</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, labore.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, maiores? Totam doloremque aliquam molestiae sapiente quas cumque consectetur, labore, ex expedita earum et inventore, obcaecati id perspiciatis quidem quisquam itaque!</p>
+            <div>
+                <button>Learn More</button>
+                <button>Book Your Trip</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Plan
