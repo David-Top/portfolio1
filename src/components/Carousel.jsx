8e3d67546be9c625bf6d56c8cd73412c82da0f9e
David-Top <david.topanto@students.unnes.ac.id>
2023-02-27 20:47:28 +0700

    finishing Carouusel component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
 
 const Carousel = () => {
-    const imagesSlide = [
+    const images = [
         {
             url: 'https://images.unsplash.com/photo-1606143704849-eb181ba1543a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmVuZGFuZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
             title: 'Rendang',
@@ -23,16 +25,47 @@ const Carousel = () => {
             title: 'Bakso',
         },
     ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const prevImage = () => {
+        const isFirstImage = currentIndex === 0;
+        const newIndex = isFirstImage ? images.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+    };
+    const nextImage = () => {
+        const isLastImage = currentIndex === images.length - 1;
+        const newIndex = isLastImage ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+    };
+
+    const goToSlide = (imageIndex) => {
+        setCurrentIndex(imageIndex);
+    };
+
   return (
-    <div className='max-w-[1400px] h-[580px] w-full m-auto py-16 px-4 relative'>
+    <div className='max-w-[1400px] h-[580px] w-full m-auto py-16 px-4 relative group'>
         {/* Backgound Images */}
-        <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500' style={{ backgroundImage: `url(${imagesSlide[0].url})`}}></div>
+        <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500' style={{ backgroundImage: `url(${images[currentIndex].url})`}}></div>
         
         {/* Left Arrow */}
-        <div></div>
+        <div className='absolute top-[50%] -translate-x-0 -translate-y-[-50%] left-5 text-2xl rounded-full p-2 group-hover:bg-black/40 text-white cursor-pointer'>
+            <BsChevronCompactLeft size={25} onClick={prevImage}/>
+        </div>
         
         {/* Right Arrow */}
-        <div></div>
+        <div className='absolute top-[50%] -translate-x-0 -translate-y-[-50%] right-5 text-2xl rounded-full p-2 group-hover:bg-black/40 text-white cursor-pointer'>
+            <BsChevronCompactRight size={25} onClick={nextImage}/>
+        </div>
+
+        {/* Bullet icons */}
+        <div className='flex top-4 justify-center py-2'>
+            {images.map((image, imageIndex) => (
+                <div className='text-2xl cursor-pointer' key={imageIndex} onClick={() => goToSlide(imageIndex)}>
+                    <RxDotFilled/>
+                </div>                
+            ))}
+        </div>
     </div>
   )
 }
